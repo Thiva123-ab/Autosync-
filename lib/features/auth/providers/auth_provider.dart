bb20d893058cf3c2c63e9c9ef914617964c283a2
Thivanka Tharuka <thivankatharuka36@gmail.com>
2026-09-01 13:49:48 +0530

    feat: initialize app entry point and implement authentication state and user role management providers

--- lib/features/auth/providers/auth_provider.dart
+++ lib/features/auth/providers/auth_provider.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/package:firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../core/constants/app_roles.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/package:firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/routing/app_router.dart';
 
