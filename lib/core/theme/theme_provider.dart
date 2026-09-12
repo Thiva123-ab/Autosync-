68c29b717e818eb58cfc245f001f60f6bac2f287
Thivanka Tharuka <thivankatharuka36@gmail.com>
2026-09-12 14:11:23 +0530

    import misssing  package

--- lib/core/theme/theme_provider.dart
+++ lib/core/theme/theme_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
   throw UnimplementedError('sharedPreferencesProvider must be overridden');
